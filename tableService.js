eaae8cfdb9ae3348dc14388370c91c234919c807
skippskipp <Scott@Melanies-MBP.westell.com>
2017-12-27 15:36:58 -0500

    Updates table builder

--- tableService.js
+++ tableService.js
@@ -16,26 +16,29 @@ snapshotToArray = function(snapshot) {
 };
 
 annotations.on('value', function(snapshot) {
-    console.log(snapshotToArray(snapshot));
+    var data = snapshotToArray(snapshot);
+    var dataArray = Array.from(data);
+    console.log(dataArray);
+    tablePut(dataArray);
 });
 
-tablePut = function addTable() {
+tablePut = function addTable(dataArray) {
     var myTableDiv = document.getElementById("database")
     var table = document.createElement('TABLE')
     var tableBody = document.createElement('TBODY')
 
-    table.border = '1'
+    // table.border = '1'
     table.appendChild(tableBody);
 
-    var heading = new Array();
+    var heading = new Array()
     heading[0] = "Title"
     heading[1] = "Authors"
     heading[2] = "Year"
 
-    var stock = new Array()
-    stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
-    stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
-    stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
+    // var stock = new Array ;
+    // stock[0] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
+    // stock[1] = new Array("Veggies", "88.625", "85.50", "85.81", "988")
+    // stock[2] = new Array("Colors", "88.625", "85.50", "85.81", "989")
 
     //TABLE COLUMNS
     var tr = document.createElement('TR');
@@ -48,12 +51,14 @@ tablePut = function addTable() {
     }
 
     //TABLE ROWS
-    for (i = 0; i < stock.length; i++) {
+    for (i = 0; i < dataArray.length; i++) {
         var tr = document.createElement('TR');
-        for (j = 0; j < stock[i].length; j++) {
+        for (j = 0; j < dataArray[i].length; j++) {
             var td = document.createElement('TD')
-            td.appendChild(document.createTextNode(stock[i][j]));
+            td.appendChild(document.createTextNode(dataArray[i][j]));
             tr.appendChild(td)
         }
+        tableBody.appendChild(tr);
       }
+    myTableDiv.appendChild(table)
     };
