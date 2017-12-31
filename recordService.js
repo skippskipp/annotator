27f0d813962c4ab817bfc5d50aa142dc4163c77f
skippskipp <Scott@Melanies-MBP.westell.com>
2017-12-31 11:59:30 -0500

    Adds logic to recordService to avoid duplicate titles

--- recordService.js
+++ recordService.js
@@ -1,17 +1,74 @@
 var myFirebase = firebase.database().ref();
 var annotations = myFirebase.child("annotations");
-var saveBtn = document.getElementById('saveBtn');
 
+snapshotToArray = function(snapshot) {
+    var returnArr = [];
+    snapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+        returnArr.push(item);
+    });
+    return returnArr;
+  };
 
-saveBtn.addEventListener('click', function() {
-  var title = document.getElementById('title').value;
-  var authors = document.getElementById('authors').value;
-  var year = document.getElementById('year').value;
-  annotations.push({
-      "title": title,
-      "authors": authors,
-      "year": year
-  }).then(function(){
-    document.location.href = 'records.html';
-  });
+foundPresent = function(title, data) {
+
+    var objArray = Object.values(data);
+    var titles = objArray.map(a => a.title);
+    var present = (titles.indexOf(title) > -1);
+
+    if (present == true) {
+      return true;
+    } else {
+      return false;
+    };
+};
+
+annotations.once('value', function(snapshot) {
+    var data = snapshotToArray(snapshot);
+
+    saveBtn.addEventListener('click', function() {
+
+      var title = document.getElementById('title').value;
+      var authors = document.getElementById('authors').value;
+      var year = document.getElementById('year').value;
+      var exists = foundPresent(title, data);
+
+      if (exists == true) {
+        alert("This title already exists!");
+        return;
+      } else {
+        annotations.push({
+            "title": title,
+            "authors": authors,
+            "year": year
+        }).then(function(){
+          document.location.href = 'records.html';
+        });
+        return;
+      };
+    });
 });
+
+
+
+
+
+
+
+
+
+
+//use set() for updating e.g. userRef.update({timestamp: 123456789});
+//var timestamp = (new Date()).getTime();
+// usersRef.update({
+//  ‘/someUserKey/lastLogin’: timestamp,
+//  ‘/adminLogs/logins/anotherUserKey/lastLogin’: timestamp
+// })
+// .catch(function (err) {
+//  console.log(‘one of these updates failed’, err);
+// });
+
+//use remove() for deleting
+
+//fruitRef.orderByKey().equalTo(“5”)  <= target a specific key on a ref
